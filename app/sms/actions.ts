bd3577899a717517c79b24ff402b6623c10d7800
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2025-02-21 00:13:12 +0900

    250221 [branch:main] 업데이트 내역
    [carrot-market 2024 업데이트 버전]
    1. #6.9 recap 까지 완료
    2. #7.0 prisma 부터 시작

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -4,7 +4,13 @@ import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -7,6 +7,7 @@ import { smsLogin } from "./actions";
 
 const initialState = {
   token: false,
+  error: undefined,
 };
 
 export default function SMSLogin() {
@@ -18,7 +19,6 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
         {state.token ? (
           <Input
             name="token"
@@ -27,9 +27,18 @@ export default function SMSLogin() {
             required
             min={100000}
             max={999999}
+            errors={state.erorr?.formErrors}
           />
-        ) : null}
-        <Button text="Verify" />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.erorr?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
