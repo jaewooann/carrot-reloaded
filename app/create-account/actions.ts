2296a1d265e046dfd1674698c60b877a52ef3ca1
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2025-02-21 15:46:40 +0900

    250221 [branch:main] 업데이트 내역
    [carrot-market 2024 업데이트 버전]
    1. #8.4 Recap 시작

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "../libs/constants";
+import db from "../libs/db";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkPasswords = ({
   password,
@@ -14,6 +19,35 @@ const checkPasswords = ({
   password: string;
   confirmPassword: string;
 }) => password === confirmPassword;
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
 
 const formSchema = z
   .object({
@@ -24,12 +58,13 @@ const formSchema = z
       })
       .trim()
       .toLowerCase()
-      .transform((username) => `🔥 ${username}`)
-      .refine(
-        (username) => !username.includes("potato"),
-        "No potatoes allowed!"
-      ),
-    email: z.string().email().toLowerCase(),
+      // .transform((username) => `🔥 ${username}`)
+      .refine(checkUniqueUsername, "이 유저네임은 이미 존재합니다."),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "이 이메일은 이미 존재합니다."),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
@@ -42,16 +77,37 @@ const formSchema = z
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
+  console.log(cookies());
   const data = {
     username: formData.get("username"),
     email: formData.get("email"),
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "delicious-karrot",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    cookie.id = user.id;
+    await cookie.save();
+
+    redirect("/profile");
   }
 }
---
+++ app/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function Profile() {
+  return <h1>너의 프로필에 온 것을 환영해!</h1>;
+}
