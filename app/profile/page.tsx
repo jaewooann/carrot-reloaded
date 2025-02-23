a49beab796bbf20bd6c3fe6c9e6a2cd05f4e0219
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2025-02-23 23:46:36 +0900

    250223 [branch:main] 업데이트 내역
    [carrot-market 2024 업데이트 버전]
    1. #9.1 Github Authentication 시작.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,37 @@
-export default function Profile() {
-  return <h1>너의 프로필에 온 것을 환영해!</h1>;
+import { notFound, redirect } from "next/navigation";
+import db from "../libs/db";
+import getSession from "../libs/session";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    await session.destroy();
+    redirect("/");
+  };
+
+  return (
+    <div>
+      <h1>Welcome! {user?.username}!</h1>
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
