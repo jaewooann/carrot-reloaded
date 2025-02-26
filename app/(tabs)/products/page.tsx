4e86a9a86d9c7efd1c7381ebe3a8af1a782f3565
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2025-02-27 00:35:00 +0900

    250227 [branch:main] 업데이트 내역
    [carrot-market 2024 업데이트 버전]
    1. 제품 리스트, 상세 스켈레톤 생성
    2. 제품 리스트 화면 구성
    3. 제품 상세 화면 구성
    4. #10.7 Pagination Actions 시작.

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,9 +1,27 @@
+import db from "@/app/libs/db";
+import ListProduct from "@/components/list-product";
 import React from "react";
 
-const Products = () => {
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return products;
+}
+
+const Products = async () => {
+  const products = await getProducts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 };
