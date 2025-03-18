df0f6cefc5063d7c4c970dfa780dc2f935fdb8cc
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2025-03-18 13:12:16 +0900

    250318 [branch:main] 업데이트 내역
    [carrot-market 2024 업데이트 버전]
    1. 섹션 #17 마무리.

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,26 +1,12 @@
-import { revalidatePath } from "next/cache";
-
-async function getData() {
-  const data = await fetch(
-    "https://nomad-movies.nomadcoders.workers.dev/movies"
-  );
-}
+import Image from "next/image";
+import oneImage from "../../public/1.jpeg";
 
 export default async function Extras() {
-  await getData();
-
-  const dispatch = async () => {
-    "use server";
-    revalidatePath("/extras");
-  };
-
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="text-6xl font-metallica">Extras!</h1>
       <h2 className="font-roboto">So much more to learn!</h2>
-      <form action={dispatch}>
-        <button>revalidate</button>
-      </form>
+      <Image src={oneImage} alt="" placeholder="blur" />
     </div>
   );
 }
